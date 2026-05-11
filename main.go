88023cd8508e3a8638ad8be8703f6586ea175788
raddva <nadyaworkspaces@gmail.com>
2026-05-11 12:23:52 +0700

    update: core main.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const nMax = 100
+
 type Mahasiswa struct {
 	NIM, Name, DosBing string
 	IsGraduated        bool
@@ -13,8 +15,8 @@ type Skripsi struct {
 	Author   Mahasiswa
 }
 
-type SkripsiList []Skripsi
-type MahasiswaList []Mahasiswa
+type SkripsiList [nMax]Skripsi
+type MahasiswaList [nMax]Mahasiswa
 
 func addSkripsi() {
 }
@@ -44,51 +46,101 @@ func statisticSkripsi() {
 }
 
 func main() {
-	var choice int
-	var s SkripsiList
-	var m MahasiswaList
+	var mainChoice, subChoice int
+	// var s SkripsiList
+	// var m MahasiswaList
 
-	fmt.Printf("%-25s\n", "SkripsIn - Sistem Informasi Inventaris Dokumen Skripsi")
-	fmt.Printf("%-25s\n", "Pilih Menu")
+	for {
+		fmt.Println("+================================================================+")
+		fmt.Printf("%-5s%-60s%-5s\n", "+", "SkripsIn - Sistem Informasi Inventaris Dokumen Skripsi", "+")
+		fmt.Println("+================================================================+")
+		fmt.Println("Pilih Menu")
+		fmt.Println("1. Kelola Data Skripsi")
+		fmt.Println("2. Pencarian Skripsi")
+		fmt.Println("3. Pengurutan Skripsi")
+		fmt.Println("4. Statistik Skripsi")
+		fmt.Println("5. Keluar")
 
-	fmt.Printf("%-25s\n", "1. Kelola Data Skripsi")
-	fmt.Printf("%-25s\n", "2. Pencarian Skripsi")
-	fmt.Printf("%-25s\n", "3. Pengurutan Skripsi")
-	fmt.Printf("%-25s\n", "4. Statistik Skripsi")
-	fmt.Printf("%-25s\n", "5. Keluar")
-
-	fmt.Scan(&choice)
-	switch choice {
-	case 1:
-		fmt.Println("Kelola Data Skripsi")
-		fmt.Println("1. Tambah Skripsi")
-		fmt.Println("2. Update Skripsi")
-		fmt.Println("3. Hapus Skripsi")
-		fmt.Scan(&choice)
-		switch choice {
-		case 1:
-			addSkripsi()
-		case 2:
-			updateSkripsi()
-		case 3:
-			deleteSkripsi()
-		default:
+		fmt.Print("Masukkan pilihan: ")
+		fmt.Scan(&mainChoice)
+		if mainChoice == 5 {
 			return
 		}
-	case 2:
-		fmt.Println("Cari Skripsi")
-		fmt.Println("1. Pencarian Sequential")
-		fmt.Println("2. Pencarian Binary")
-		fmt.Scan(&choice)
-	case 3:
-		fmt.Println("Urutkan Skripsi")
-		fmt.Println("1. Selection Sort")
-		fmt.Println("2. Insertion Sort")
-		fmt.Scan(&choice)
-	case 4:
-		fmt.Println("Statistik Skripsi")
-		statisticSkripsi()
-	default:
-		return
+		switch mainChoice {
+		case 1:
+			fmt.Println("+===========================+")
+			fmt.Printf("%-5s%-23s%-5s\n", "+", "Kelola Data Skripsi", "+")
+			fmt.Println("+===========================+")
+			fmt.Println("0. Kembali ke Menu Utama")
+			fmt.Println("1. Tambah Skripsi")
+			fmt.Println("2. Update Skripsi")
+			fmt.Println("3. Hapus Skripsi")
+			fmt.Print("Masukkan pilihan: ")
+			fmt.Scan(&subChoice)
+			if subChoice == 0 {
+				continue
+			}
+			switch subChoice {
+			case 1:
+				addSkripsi()
+			case 2:
+				updateSkripsi()
+			case 3:
+				deleteSkripsi()
+			default:
+				return
+			}
+		case 2:
+			fmt.Println("+===========================+")
+			fmt.Printf("%-5s%-23s%-5s\n", "+", "Cari Skripsi", "+")
+			fmt.Println("+===========================+")
+			fmt.Println("0. Kembali ke Menu Utama")
+			fmt.Println("1. Pencarian Sequential")
+			fmt.Println("2. Pencarian Binary")
+			fmt.Print("Masukkan pilihan: ")
+			fmt.Scan(&subChoice)
+			if subChoice == 0 {
+				continue
+			}
+			switch subChoice {
+			case 1:
+				findSkripsiSequential()
+			case 2:
+				findSkripsiBinary()
+			default:
+				return
+			}
+		case 3:
+			fmt.Println("+===========================+")
+			fmt.Printf("%-5s%-23s%-5s\n", "+", "Urutkan Skripsi", "+")
+			fmt.Println("+===========================+")
+			fmt.Println("0. Kembali ke Menu Utama")
+			fmt.Println("1. Selection Sort")
+			fmt.Println("2. Insertion Sort")
+			fmt.Print("Masukkan pilihan: ")
+			fmt.Scan(&subChoice)
+			if subChoice == 0 {
+				continue
+			}
+			switch subChoice {
+			case 1:
+				sortSkripsiSelection()
+			case 2:
+				sortSkripsiInsertion()
+			default:
+				return
+			}
+		case 4:
+			fmt.Println("+===========================+")
+			fmt.Printf("%-5s%-23s%-5s\n", "+", "Statistik Skripsi", "+")
+			fmt.Println("+===========================+")
+			statisticSkripsi()
+			continue
+		case 5:
+			return
+		default:
+			fmt.Print("Masukkan pilihan: ")
+			fmt.Scan(&mainChoice)
+		}
 	}
 }
